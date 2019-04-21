d6fe9b48fabb19a38756a622fe8755f41e9fa78c
Chris Troutner <chris.troutner@gmail.com>
2019-04-21 07:07:43 -0700

    Adding tor hidden service docker container

--- cleanup-images.sh
+++ cleanup-images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# This script is used to cleanup orphaned Docker images. These images are
+# naturally created when developing. They need to be deleted to clear up
+# disk space.
 
 # Remove all untagged docker images.
 docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
-
